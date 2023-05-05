756cb8c2e27b377d7302dada3f3cc58f6517cd2e
Uthmanbello <uthmanbello88@gmail.com>
2023-05-05 16:25:40 +0100

    Create custom methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
-  has_many :likes 
+  has_many :likes
+
+  def recent_posts(n)
+    posts.order(created_at: :desc).limit(n)
+  end
 end
