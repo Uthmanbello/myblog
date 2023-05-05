6c60cf91b36d6ffad06319676f087994ade5d6a2
Uthmanbello <uthmanbello88@gmail.com>
2023-05-05 18:24:45 +0100

    Add foreign keys to likes and comments

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 # Top-level documentation comment for Like module
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
   after_create :update_post_likes_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # Top-level documentation comment for User module
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def recent_posts(num)
     posts.order(created_at: :desc).limit(num)
