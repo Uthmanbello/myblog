43b5fb039477467b383bebd4f38640310b92d724
Uthmanbello <uthmanbello88@gmail.com>
2023-05-08 13:32:21 +0100

    Create routes for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/users', to: 'users#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/users/:id', to: 'users#show'
+
+  get '/users/:user_id/posts', to: 'posts#index'
+
+  get '/users/:user_id/posts/:id', to: 'posts#show'
 end
