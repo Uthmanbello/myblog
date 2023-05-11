ed8041c6975506bd087c4378335903e4844ca364
Uthmanbello <uthmanbello88@gmail.com>
2023-05-11 14:39:17 +0100

    Fix remote errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Top-level documentation comment for PostsController module
 class PostsController < ApplicationController
   def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Top-level documentation comment for UsersController module
 class UsersController < ApplicationController
   def index
