ba23a32acd4576dfac19b86328ee7cf9b0f4e445
Uthmanbello <uthmanbello88@gmail.com>
2023-05-13 19:50:37 +0100

    Fix remote errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Top-level documentation comment for CommentsController module
 class CommentsController < ApplicationController
   def new
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Top-level documentation comment for LikesController module
 class LikesController < ApplicationController
   def create
