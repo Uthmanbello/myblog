e098f913e3071c5b16abc73391f6c03c7a9719c4
Uthmanbello <uthmanbello88@gmail.com>
2023-05-13 19:31:44 +0100

    Setup new post page

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,6 @@
 Rails.application.routes.draw do
   root 'users#index'
   get '', to: 'users#index'
-  post 'users/:user_id/posts/:id/like', to: 'posts#like', as: 'like_user_post'
-
-
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
-  end
-
-  resources :posts, only: [:index, :show]
-  
-
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
-      post 'like', on: :member
-    end
-  end
-
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
@@ -26,5 +10,4 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end 
-  
 end
