b1b2ea028712b578ad74461138e1483bdd5bfdf0
Uthmanbello <uthmanbello88@gmail.com>
2023-05-16 22:32:10 +0100

    Secure app from n+1 problems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 # Top-level documentation comment for UsersController module
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    @users = User.order(created_at: :asc)
+    @users = User.includes(:posts).order(created_at: :asc)
   end
 
   def show
