dc429fd8d13cab4af187b351f603bac425d62b6d
Uthmanbello <uthmanbello88@gmail.com>
2023-05-16 23:24:24 +0100

    Remove current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 # Top-level documentation comment for ApplicationController module
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-
-  def current_user
-    @current_user = User.first
-  end
 end
