0931dc99ae50a67c284fa3a0fdbb05e25168d1e4
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-05-16 17:04:03 -0300

    Create initial seeds content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,39 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create 10 users with randomized data
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    photo: Faker::Avatar.image,
+    bio: Faker::Lorem.sentence(word_count: 10)
+  )
+end
+
+# Create 50 posts with randomized data
+50.times do
+  Post.create!(
+    title: Faker::Lorem.sentence(word_count: 3),
+    text: Faker::Lorem.paragraph(sentence_count: 10),
+    author: User.order(Arel.sql('RANDOM()')).first
+  )
+end
+
+# Create 100 comments with randomized data
+100.times do
+  Comment.create!(
+    text: Faker::Lorem.paragraph(sentence_count: 10),
+    author: User.order(Arel.sql('RANDOM()')).first,
+    post: Post.order(Arel.sql('RANDOM()')).first
+  )
+end
+
+# Create 200 likes with randomized data
+500.times do
+  user = User.order(Arel.sql('RANDOM()')).first
+  post = Post.order(Arel.sql('RANDOM()')).first
+  # But only if the user hasn't liked the post yet
+  unless Like.exists?(author: user, post: post)
+    Like.create!(
+      author: user,
+      post:
+    )
+  end
+end
