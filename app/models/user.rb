05a1b871c60d453d19deda917846f2476832e6af
Uthmanbello <uthmanbello88@gmail.com>
2023-05-17 00:07:04 +0100

    Create migration for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # Top-level documentation comment for User module
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
