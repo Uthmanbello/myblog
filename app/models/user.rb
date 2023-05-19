92edeec0d4dd461cffa2e239bc9f60d835231459
Uthmanbello <uthmanbello88@gmail.com>
2023-05-19 01:48:36 +0100

    Add confirmable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,5 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :email, confirmation: true
 end
